bf4c35e08415c9768e02279ca687b4e726122cf3
Miyu Masayama <miyumasayama@Miyus-Laptop.lan>
2024-11-05 17:33:36 +0000

    設定ファイルの変更

--- knexfile.js
+++ knexfile.js
@@ -34,18 +34,19 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
+    client: "mysql2",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./migrations",
     },
   },
 };
